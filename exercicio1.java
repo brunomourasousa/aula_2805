52268bf3f36f8f9889e4ac1d206e839927b8f936
Bruno Moura <164107317+brunomourasousa@users.noreply.github.com>
2024-06-11 21:17:06 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,27 @@
+package testetarget;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int indice = 13;
+		int soma = 2;
+		int k = 3;
+		
+		while(k <indice) {
+			 soma = soma + k;
+			 
+			 System.out.println(soma);	
+			
+		}k++;
+		
+		
+		
+	
+		
+		
+		
+	}
+
+}
